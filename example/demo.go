1215ff290b42c031976f79a673918dc110dd8cfe
李清波 <252588119@qq.com>
2023-08-03 15:42:36 +0800

    firstCommit

---
+++ example/demo.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/qinxin0720/alisms-go/SmsClient"
+)
+
+const (
+	accessKeyID     = "yourAccessKeyId"
+	secretAccessKey = "yourAccessKeySecret"
+)
+
+func main() {
+	sc, err := SmsClient.NewSMSClient(accessKeyID, secretAccessKey)
+	if err != nil {
+		return
+	}
+	statusCode, _, _ := sc.SendSMS(SmsClient.Params{"1500000000", "阿里云短信", "SMS_000000", `{"code":"12345"}`})
+	if statusCode == http.StatusOK {
+		log.Println("发送成功")
+	} else {
+		log.Println("发送失败")
+	}
+}
